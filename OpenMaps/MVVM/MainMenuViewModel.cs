ae98a6c3fba868f0d58dbf7296366adb2fcd489a
Zander Baker <zanderbaker100@gmail.com>
2022-05-03 21:09:07 -0700

    Add basic MainWindow menu
    [Addition]: Create MainMenuViewModel
    [Modification]: Add topbar menu in MainWindow

---
+++ OpenMaps/MVVM/MainMenuViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.ObjectModel;
+using System.Windows.Media;
+
+namespace OpenMaps.MVVM;
+
+public class MainMenuViewModel
+{
+    public ObservableCollection<RecentFileInfo> RecentFiles { get; }
+
+    public MainMenuViewModel()
+    {
+        RecentFiles = new ObservableCollection<RecentFileInfo>();
+    }
+}
+
+public readonly record struct RecentFileInfo(string ShortName, string FullPath, ImageSource Image);
--- OpenMaps/MainWindow.xaml.cs
+++ OpenMaps/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using OpenMaps.MVVM;
 
 namespace OpenMaps
 {
@@ -20,9 +21,15 @@ namespace OpenMaps
     /// </summary>
     public partial class MainWindow : Window
     {
+        public MainMenuViewModel ViewModel { get; }
+        
         public MainWindow()
         {
             InitializeComponent();
+
+            ViewModel = (MainMenuViewModel)DataContext;
+            
+            ViewModel.RecentFiles.Add(new RecentFileInfo("Test", @"C:\Test", new BitmapImage()));
         }
     }
 }
